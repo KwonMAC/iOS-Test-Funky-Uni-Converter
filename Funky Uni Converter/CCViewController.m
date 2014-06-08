fa2f2ce1f2bc25a259a679504df88575c7df3d9f
Kwon <kwontaehun@Kwonui-Mac-mini.local>
2014-06-08 10:17:38 +0900

    first commit

--- Funky Uni Converter/CCViewController.m
+++ Funky Uni Converter/CCViewController.m
@@ -17,13 +17,37 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    int x = 5;
+    int y = 20;
+    int z = -2;
+    
+    
+    // operators include: + - * /
+    
+    int additionAnswer = x + y;
+    int multiplicationAnswer = y * z;
+    
+    float heightOfEverstBaseCamp = 16900.3;
+    float heightOfEverst = 29029;
+    
+    float distanceToTravel = heightOfEverst - heightOfEverstBaseCamp;
+    distanceToTravel = distanceToTravel - 1000;
+    
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    NSLog(@"didRecieveMomoryWarning method is evaluating");
 }
 
+- (IBAction)ConvertUnits:(UIButton *)sender
+{
+    float numberOfBills = [self.numberOfBillsTextField.text floatValue];
+    float numberOfFootballFields = numberOfBills / 91440;
+    self.numberOfBillsLabel.text = [NSString stringWithFormat:@"%f", numberOfFootballFields];
+}
 @end
